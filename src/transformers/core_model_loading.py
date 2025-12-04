c284b4269ec34efa9fb58d9f71053cc6e458bf93
DoubleMathew <mmathew23@gmail.com>
2025-12-03 22:07:22 -0600

    debugging

--- src/transformers/core_model_loading.py
+++ src/transformers/core_model_loading.py
@@ -823,7 +823,7 @@ def convert_and_load_state_dict_in_model(
                 mapping.quantization_operation = hf_quantizer.get_quantize_ops()
 
             _dtype = dtype
-            if hf_quantizer and hf_quantizer.pre_quantized and (original_key != renamed_key and 'model.'+original_key != renamed_key):
+            if hf_quantizer and hf_quantizer.pre_quantized and (original_key != renamed_key and 'model.'+original_key != renamed_key) and False:
                 # if the key was renamed as it is not available in the state dict otherwise, it means that we are deserializing it,
                 # so we need to make sure to load the tensor with the same dtype from the checkpoint
                 # TODO: make the condition more srict for native fp8 model such as qwen2moe fp8
